edc987b71105f441a7a9a3fe8534f28f9b5c0e25
magarteem <fmf311@mail.ru>
2022-09-03 17:10:52 +0300

    pw3

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { AppRouter } from "./core/router/AppRouter";
+import { ButtonInstallPwa } from "./modules/pwa/ButtonInstallPwa";
 import "./styles/index.scss";
 
 export const App = () => {
   return (
     <div className="App">
+
+      {/* for test */}
+      <ButtonInstallPwa />
       {/* for test */}
       <AppRouter />
     </div>
