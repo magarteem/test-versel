ece75289dff722c1652373ef2a9f481ed1ce8dea
magarteem <fmf311@mail.ru>
2022-09-13 01:35:17 +0300

    add 4

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const _api = axios.create({
-  baseURL: "https://631ee4c522cefb1edc3cffbd.mockapi.io",
-  //baseURL: process.env.REACT_APP_API_URL,
+  //baseURL: "https://631ee4c522cefb1edc3cffbd.mockapi.io",
+  baseURL: process.env.REACT_APP_API_URL,
 });
