55784543ddb3ab5dd22b988846ba583647689d86
magarteem <fmf311@mail.ru>
2022-09-13 04:32:30 +0300

    add 9

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,6 @@ import s from './styles/tempStyle.module.scss';
 
 export const Home = () => {
   return (
-    <div className={s.tempStyle}>Home<br /> (в разработке-4)</div>
+    <div className={s.tempStyle}>Home<br /> (в разработке-5)</div>
   )
 }
